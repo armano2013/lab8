d5b59a291937653a4d0ecb52fd732f81868c1d92
Demonshae Glover <dshaeglover@gmail.com>
2017-07-18 19:12:02 -0400

    made changes to js file

--- index.js
+++ index.js
@@ -2,7 +2,20 @@ $(function(){
   $('.seat').click(function(){
    $('#chart').toggle();
 });
-$('#form').click(funtion(){
+$('form').click(function(){
   $('form').toggle();
+})
+$(form).on(’submit’, function(){
+ });
 });
-});
+
+// event handerler funciton
+// $('form').submit(function(evt){
+// evt.preventDefault();
+// var firstName = $(this).find('#first-Name');
+// var lastName = $(this).find('#last-Name');
+// var seatNumber = $(this).find('#seat-Number').val();
+// console.log(firstName, lastName, seatNumber);
+// });
+
+//if seat is taken, toggle class and change color
