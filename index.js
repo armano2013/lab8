3511fc4cf4dd52c5380f99375c681dc28c45a014
Timothy Batty <tbatty7@gmail.com>
2017-07-20 00:31:18 -0400

    added functionality and cut out most of the styling

--- index.js
+++ index.js
@@ -2,29 +2,57 @@ var reservedSeats = {};
 
 $(function(){
 
-  $('form').on('submit', function(event){
-    event.preventDefault();
-
-  });
-
-
 $('.seat').click(function(){
-  $('#form').show();
+	$('#form').toggle();
+	$('#chart').toggle();
+	var seatNum = $(this).text();
+	console.log("Seat number " + seatNum);
+	$('#seatNum').val(seatNum);
  });
-$('#submit').click(function(){
-  $('#form').toggle();
-  $('#chart').toggle();
- });
-$('#cancel').click(function(){
-  $('#form').toggle();
-  $('#chart').toggle();
 
- // reservedSeats[seatName] = {
- //   customerName: name,
- //   number: phoneNumber
- // }
+$('form').on('submit', function(event){
+	event.preventDefault();
+	$('#form').toggle();
+	$('#chart').toggle();
 
+	var name = $(this).find('#name').val();
+	var seatNumber = $(this).find('#seatNum').val();
+	var phoneNumber = $(this).find('#phone').val();
+
+	console.log(seatNumber + "  " + name + "   " + phoneNumber);
+
+	$('#' + seatNumber).addClass('reserved').html('Reserved');  // this takes a number from the seat and changes 
+	// the id to the correct id because all the ids of the seats are “1” or “4”, etc.  
+	// It then takes the text and replaces it with the word ‘reserved’.
+	$(this).trigger('reset');  //This clears the fields in the form.
+	reservedSeats[seatNumber] = {
+		customerName: name,
+		number: phoneNumber
+	}
+
+	console.log(reservedSeats);
  });
+$('#cancel').click(function(event){
+	event.preventDefault();
+	$('#form').toggle();
+	$('#chart').toggle();
+ });
+
+$('div').hover(function() {
+// What happens when I hover over the div
+	var $div = $(this);
+	if($div.hasClass('reserved')){
+	$div.html(reservedSeats[$div.attr('id')].customerName);
+	}
+	}, function(){
+//What happens when I hover out of the div
+	var $div = $(this);
+	if($div.hasClass('reserved')){
+		$div.html('reserved');
+	}
+});
+
+
 });
 
 
